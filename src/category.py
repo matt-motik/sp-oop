37462cacf0a9c196e30ae542e689f08fa1fb3db4
Bakirov Matvey <mabakirov@gmail.com>
2026-05-26 13:26:59 +0300

    feat: add product.py base realization - add category.py base realization - add imports to main.py

---
+++ src/category.py
@@ -0,0 +1,22 @@
+"""Модуль Категории"""
+from product import Product
+
+
+
+class Category:
+    name: str
+    description: str
+    products: list[Product]
+
+    category_count: int = 0
+    product_count: int = 0
+
+
+
+    def __init__(self, name: str, description: str, products: list[Product] | None) -> None:
+        self.name = name
+        self.description = description
+        self.products = products if products is not None else []
+        Category.category_count +=1
+        Category.product_count += len(self.products)
+
